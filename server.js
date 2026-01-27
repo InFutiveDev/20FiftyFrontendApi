4068ae530a142f705d0b6c4515f74d69ec2fd870
vikash <93303641+InFutiveDev@users.noreply.github.com>
2026-01-27 17:59:15 +0530

    update port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
